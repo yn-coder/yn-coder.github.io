b524b7afc60c3111a76b07f36fdfb6037f637c37
yn-coder <yn.coder@gmail.com>
2017-08-08 01:14:57 +0300

    Create googlemaps_test_data.js

---
+++ samples/googlemaps_test_data.js
@@ -0,0 +1,8 @@
+
+["cid=5511975034435<br>imei=357636063684860<br>2017-08-07 13:57:43<br><a href='http://maps.google.com/maps?q=-21.198389,-41.913632' target='_BLANK' title='Google Maps'>Google Maps</a>", -21.198389, -41.913632, "greenmarker.png", 1],
+
+["cid=5511995663840<br>imei=357636062222310<br>2017-08-07 12:43:17<br><a href='http://maps.google.com/maps?q=-13.004046,-38.458964' target='_BLANK' title='Google Maps'>Google Maps</a>", -13.004046, -38.458964, "greenmarker.png", 2],
+
+["cid=5519998677213<br>imei=011707008352890<br>2017-08-07 11:07:37<br><a href='http://maps.google.com/maps?q=-28.32734,-53.971297' target='_BLANK' title='Google Maps'>Google Maps</a>", -28.32734, -53.971297, "greenmarker.png", 3],
+
+["cid=5519998247271<br>imei=357636062693550<br>2017-08-07 11:06:52<br><a href='http://maps.google.com/maps?q=-22.876369,-42.014614' target='_BLANK' title='Google Maps'>Google Maps</a>", -22.876369, -42.014614, "greenmarker.png", 4] 
